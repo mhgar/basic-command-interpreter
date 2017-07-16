23e068fdc81bd5d9f058befa2269b5e522a988b2
mhgar <hunter.shk.nz@gmail.com>
2017-07-17 03:56:07 +1200

    Add files via upload
    fixed formatting

--- Main.java
+++ Main.java
@@ -9,58 +9,58 @@ import interpreter.CommandLine;
 
 public class Main
 {
-  public static void main(String[] args)
-  {
-	CommandLine cl = new CommandLine();
-	
-    Command cmd1 = new Command("print", (Command c) -> print(c));
-	Command cmd2 = new Command("turnip", (Command c) -> printReverse(c));
-	Command cmd3 = new Command("help", (Command c) -> printCommands(c));
-	Command cmd4 = new Command("sort", (Command c) -> sort(c));
-	
-	cl.register(cmd1);
-	cl.register(cmd2);
-	cl.register(cmd3);
-	cl.register(cmd4);
-    
-	// Read commands from the user and execute them with parametres.
-    while (true) {
-     	System.out.print("> ");
-    	Scanner scan = new Scanner(System.in);
-      	String s = scan.nextLine();
-      	cl.exec(s);
+    public static void main(String[] args)
+    {
+        CommandLine cl = new CommandLine();
+        
+        Command cmd1 = new Command("print", (Command c) -> print(c));
+        Command cmd2 = new Command("turnip", (Command c) -> printReverse(c));
+        Command cmd3 = new Command("help", (Command c) -> printCommands(c));
+        Command cmd4 = new Command("sort", (Command c) -> sort(c));
+        
+        cl.register(cmd1);
+        cl.register(cmd2);
+        cl.register(cmd3);
+        cl.register(cmd4);
+        
+        // Read commands from the user and execute them with parametres.
+        while (true) {
+            System.out.print("> ");
+            Scanner scan = new Scanner(System.in);
+            String s = scan.nextLine();
+            cl.exec(s);
+        }
     }
-  }
-  
-  public static void print(Command cmd) 
-  { 
-	cmd.copyArgs().forEach((s) -> cmd.Log(s + " "));
-    System.out.println();
-  }
-  
-  public static void printReverse(Command cmd)
-  {
-	ArrayList<String> args = cmd.copyArgs();
-	
-	/* Seems silly to use a lambda, but it looks pretty cool. */
-	Collections.reverse(args);
-	args.forEach((String s) -> {
-		for (int l = s.length() - 1; l >= 0; l--) 
-			cmd.Log(s.charAt(l));
-		cmd.Log(" ");
-	});
-	cmd.Log("\n");
-  }
-  
-  public static void printCommands(Command cmd)
-  {
-	cmd.getOwner().forEachCommand(c -> cmd.Log(c.getName() + "\n"));
-  }
-  
-  public static void sort(Command cmd)
-  {
-	cmd.copyArgs().stream()
-				  .sorted()
-		          .forEach((s) -> cmd.Log(s + "\n"));
-  }
+
+    public static void print(Command cmd) 
+    { 
+        cmd.copyArgs().forEach((s) -> cmd.Log(s + " "));
+        System.out.println();
+    }
+
+    public static void printReverse(Command cmd)
+    {
+        ArrayList<String> args = cmd.copyArgs();
+        
+        /* Seems silly to use a lambda, but it looks pretty cool. */
+        Collections.reverse(args);
+        args.forEach((String s) -> {
+            for (int l = s.length() - 1; l >= 0; l--) 
+            cmd.Log(s.charAt(l));
+            cmd.Log(" ");
+        });
+        cmd.Log("\n");
+    }
+
+    public static void printCommands(Command cmd)
+    {
+        cmd.getOwner().forEachCommand(c -> cmd.Log(c.getName() + "\n"));
+    }
+
+    public static void sort(Command cmd)
+    {
+        cmd.copyArgs().stream()
+                      .sorted()
+                      .forEach((s) -> cmd.Log(s + "\n"));
+    }
 }
--- interpreter/Command.java
+++ interpreter/Command.java
@@ -5,49 +5,49 @@ import java.util.ArrayList;
 
 public class Command
 {
-  private Consumer<Command> f;
-  private Consumer<String> logF;
-  private String name;
-  // ArrayList because arrays don't have nice iterators and such
-  private ArrayList<String> args; 
-  private CommandLine owner;
-  
-  public Command(String name, Consumer<Command> f)
-  {
-    this.f = f;
-    this.name = name;
-	setLogFunction(System.out::print);
-  }
-  
-  /* Execute input from the interpreter. */
-  public void exec(ArrayList<String> args)
-  {
-	  this.args = args;
-  	  if (f != null) f.accept(this);
-  }
-  
-  public void Log(String message)
-  {
-	if (logF != null) {
-		logF.accept(message);
-	}
-  }
-  
-  public void Log(char c)
-  {
-	Log(Character.toString(c));
-  }
-  
-  /* Set the owning interpreter. Can be used to find other commands or prehaps 
-   * even call a log callback. */
-  public void setOwner(CommandLine line) { owner = line; }  
-  public CommandLine getOwner() { return owner; }
-  public String getName() { return name; }
-  
-  /* Set the logging function to something more interesting than 
-   * System.out::print. */
-  public void setLogFunction(Consumer<String> func) { logF = func; }
-  
-  /* Copy all arguments passed from the interpreter. Copying is okay for now. */
-  public ArrayList<String> copyArgs() { return new ArrayList<String>(args); }
+    private Consumer<Command> f;
+    private Consumer<String> logF;
+    private String name;
+    // ArrayList because arrays don't have nice iterators and such
+    private ArrayList<String> args; 
+    private CommandLine owner;
+
+    public Command(String name, Consumer<Command> f)
+    {
+        this.f = f;
+        this.name = name;
+        setLogFunction(System.out::print);
+    }
+
+    /* Execute input from the interpreter. */
+    public void exec(ArrayList<String> args)
+    {
+        this.args = args;
+        if (f != null) f.accept(this);
+    }
+
+    public void Log(String message)
+    {
+        if (logF != null) {
+            logF.accept(message);
+        }
+    }
+
+    public void Log(char c)
+    {
+        Log(Character.toString(c));
+    }
+
+    /* Set the owning interpreter. Can be used to find other commands or prehaps 
+     * even call a log callback. */
+    public void setOwner(CommandLine line) { owner = line; }  
+    public CommandLine getOwner() { return owner; }
+    public String getName() { return name; }
+
+    /* Set the logging function to something more interesting than 
+     * System.out::print. */
+    public void setLogFunction(Consumer<String> func) { logF = func; }
+
+    /* Copy all arguments passed from the interpreter. Copying is okay for now. */
+    public ArrayList<String> copyArgs() { return new ArrayList<String>(args); }
 }
